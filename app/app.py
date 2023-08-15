a5732bbddf0dbd4671b50208b5636593772effae
Johannes Zumthurm <nimro27@gmx.net>
2023-08-15 10:51:02 +0200

    first commit

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from resources.whoishome import WhoIsHome
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+class StaticWhoIsHome(Resource):
+    def get(self):
+        return [{'name': 'Horacio'}, {'name': 'Johannes'}]
+
+api.add_resource(StaticWhoIsHome, '/whoishomestatic')
+
+api.add_resource(WhoIsHome, '/whoishome')
+
+if __name__ == '__main__':
+    app.run(debug=True)
